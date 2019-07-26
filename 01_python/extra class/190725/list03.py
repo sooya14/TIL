ee14661ec87781f075c2cef258bfa10f2466c776
sooya <soosmile9653@gmail.com>
2019-07-26 19:13:16 +0900

    python review, extraclass | 190726

---
+++ 01_python/extra class/190725/list03.py
@@ -0,0 +1,20 @@
+# list methods 
+
+my_list = [1, 2, 3]
+
+# my_list 를 바꾼다. 
+my_list.append(4)  # my_list == [1, 2, 3, 4]
+my_list.extend()  # 
+my_list.insert()
+my_list.remove()
+my_list.pop()
+my_list.sort()
+my_list.reverse()
+
+# my_list 가 바뀌지 않는다. => 조회한다. 
+my_list.index(x)
+my_list.count()
+
+
+
+
