e9a360c24aa2a2eca107d30e2322c1cf7e8064b8
sooya <soosmile9653@gmail.com>
2019-10-07 13:54:49 +0900

    implement Read with MTV | 191007

--- 06_django_advance/01_DJANGO_RECAP/board/urls.py
+++ 06_django_advance/01_DJANGO_RECAP/board/urls.py
@@ -5,4 +5,18 @@ app_name = 'board'
 
 urlpatterns = [
     path('', views.index, name='index'), # /board/ == board:index
+
+    # Read 글 목록(list) render
+    path('articles/', views.list, name='list'),
+    # Read 글 상세 (detail) render
+    path('articles/<int:id>/', views.detail, name='detail'),
+
+    # Create 글 쓰기(new) render
+    # Create 글 저장 (create) 
+
+    # Update 글 수정쓰기 (edit) render
+    # Update 글 실제수정 (update)
+
+    # Delete 글 삭제 (delete)
 ]
+    
--- 06_django_advance/01_DJANGO_RECAP/board/views.py
+++ 06_django_advance/01_DJANGO_RECAP/board/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from .models import Article
 
-# Create your views here.
 
 def index(request):
     return render(request, 'board/index.html')
+
+def list(request):
+    articles = Article.objects.all()  
+    # 다 가져와주세요 / [<A1>, <A2>,  ...] 객체가 여러개 리스트 비슷하게 return 이 된다.(변수명을 복수로 짓는 이유)
+    # context = {'articles': articles} 하고 context
+    return render(request, 'board/list.html', {
+        'articles': articles,
+    })
+
+def detail(request, id):
+    article = Article.objects.get(id=id) # <A_id> 
+    return render(request, 'board/detail.html', {
+        'article': article, 
+    })
+  
+
